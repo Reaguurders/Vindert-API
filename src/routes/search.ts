b352742c8b91634449dd492505a022c20d4d3cb8
Ruben Rutten <ruben@rubenrutten.nl>
2019-01-07 20:08:59 +0100

    Added new search functionality

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -5,6 +5,10 @@ import Post from "../models/posts/post.model";
 import { sequelize } from "../core/sequelize";
 import { Op } from "sequelize";
 import PostTag from "../models/posts/post-tag.model";
+import { AppError } from "../errors/app.error";
+import moment = require("moment");
+
+const parser = require("search-query-parser");
 
 const log = debug("app:routes:search");
 
@@ -14,39 +18,239 @@ router.get("/", async (ctx) => {
 	log(`searching request`);
 
 	if (!ctx.query.q || ctx.query.q.length < 3) {
-		throw new Error("search too short");
+		throw new AppError("search too short");
 	}
 
-	let posts = await Post.findAll({
-		attributes: {
-			include: [
-				sequelize.literal(`ts_rank("searchable", websearch_to_tsquery('dutch_nostop', ${sequelize.escape(ctx.query.q)})) AS "rank"`)
-			],
-			exclude: [
-				"rawData",
-				"createdAt",
-				"updatedAt"
-			]
-		},
-		where: {
-			[Op.and]: [
-				sequelize.literal(`"searchable" @@ websearch_to_tsquery('dutch_nostop', ${sequelize.escape(ctx.query.q)})`)
-			]
-		},
-		order: [
-			sequelize.literal(`"rank" DESC`),
-			["postedAt", "DESC"]
-		],
-		include: [{
-			model: PostTag
-		}],
-		limit: 20
+	const options = {
+		nsfw: null,
+		type: ["video", "image", "audio"],
+		query: null,
+		before: null,
+		after: null,
+		sort: ["postedAt", "desc"],
+		page: 0
+	};
+
+	const parsed = parser.parse(ctx.query.q || "", {
+		tokenize: true,
+		offsets: false,
+		keywords: [
+			"nsfw",
+			"type",
+			"before",
+			"voor",
+			"after",
+			"na",
+			"sort",
+			"sorteren"
+		]
+	});
+
+	if (parsed.text && Array.isArray(parsed.text)) {
+		parsed.text = parsed.text.join(" ");
+	}
+
+	if (!parsed || !parsed.text || parsed.text.length < 3) {
+		throw new AppError("search too short");
+	}
+
+	// set the query words
+	options.query = parsed.text;
+
+	// parse nsfw
+	let nsfw = ctx.query.nsfw || parsed.nsfw;
+	const yes = ["true", "yes", "ja"];
+	const no = ["false", "no", "nee"];
+	if (nsfw && [...yes, ...no].indexOf(nsfw) !== -1) {
+		if (yes.indexOf(nsfw) !== -1) {
+			options.nsfw = true;
+		} else if (no.indexOf(nsfw) !== -1) {
+			options.nsfw = false;
+		}
+	}
+
+	// parse type
+	let type = [];
+	if (ctx.query.type) {
+		type.push(...ctx.query.type.split(",").filter(t => t && t.length > 1));
+	} else if (parsed.type) {
+		if (Array.isArray(parsed.type)) {
+			type.push(...parsed.type);
+		} else {
+			type.push(parsed.type);
+		}
+	}
+	if (type.length > 0) {
+		const types = ["video", "image", "audio", "foto"];
+		if (type.every(t => types.indexOf(t) !== -1)) {
+			options.type = type.map(t => t === "foto" ? "image" : t);
+		}
+	}
+
+	// parse before
+	let before = ctx.query.before || parsed.before || parsed.voor;
+	if (before) {
+		// check for full date
+		let date = moment(before, "DD-MM-YYYY");
+
+		// check for month and year
+		if (!date.isValid()) {
+			date = moment(before, "MM-YYYY");
+		}
+
+		// check for only year
+		if (!date.isValid()) {
+			date = moment(before, "YYYY");
+		}
+
+		if (date.isValid()) {
+			options.before = date;
+		}
+	}
+
+	// parse after
+	let after = ctx.query.after || parsed.after || parsed.na;
+	if (after) {
+		// check for full date
+		let date = moment(after, "DD-MM-YYYY");
+
+		// check for month and year
+		if (!date.isValid()) {
+			date = moment(after, "MM-YYYY");
+		}
+
+		// check for only year
+		if (!date.isValid()) {
+			date = moment(after, "YYYY");
+		}
+
+		if (date.isValid()) {
+			options.after = date;
+		}
+	}
+
+	let sort = ctx.query.sort || parsed.sort || parsed.sorteren;
+	if (sort) {
+		let direction = "desc";
+		if (sort.indexOf("+")) {
+			direction = "asc";
+		}
+
+		sort = sort.replace(/\+|\-/g, "");
+
+		if (["score", "date", "datum", "kudos", "views", "comments", "reaguursels"].indexOf(sort) !== -1) {
+			if (sort === "datum") {
+				sort = "date";
+			} else if (sort === "reaguursels") {
+				sort = "comments";
+			}
+
+			options.sort = [sort, direction];
+		}
+	}
+
+	// build the query
+	let score;
+	if (options.sort[0] === "score") {
+		score = `ts_rank(p."searchable", websearch_to_tsquery('dutch_nostop', :query)) AS "score"`;
+	}
+
+	let where = [];
+	if (options.nsfw !== null) {
+		where.push(`p."nsfw" = ${options.nsfw}`);
+	}
+
+	if (options.type.length > 0) {
+		where.push(`((${options.type.map(t => {
+			return `p."${t}Count" > 0`;
+		}).join(`) OR (`)}))`);
+	}
+
+	if (options.before) {
+		where.push(`p."postedAt" < :before`);
+	}
+
+	if (options.after) {
+		where.push(`p."postedAt" > :after`);
+	}
+
+	where.push(`"searchable" @@ websearch_to_tsquery('dutch_nostop', :query)`);
+
+	let order;
+	switch (options.sort[0]) {
+		case "date":
+			order = `p."postedAt" ${options.sort[1]}`;
+			break;
+		default:
+			order = `"${options.sort[0]}" ${options.sort[1]}`;
+	}
+
+	const results = await sequelize.query(`
+		SELECT
+			p."id",
+			p."dumpertId",
+			p."title",
+			p."description",
+			p."thumbnail",
+			p."postedAt",
+			p."nsfw",
+			p."videoCount",
+			p."imageCount",
+			p."audioCount"${score ? `,
+			${score}` : ""},
+			string_agg(pt."tag", ',') AS "tags",
+			ph."views",
+			ph."kudos",
+			ph."comments"
+		FROM
+			"posts" p
+		LEFT JOIN
+			"postTags" pt
+			ON
+				pt."postId" = p."id"
+		LEFT JOIN
+			(
+				SELECT
+					DISTINCT ON("postId") "postId",
+					"views",
+					"kudos",
+					"comments"
+				FROM
+					"postHistories"
+				ORDER BY
+					"postId" DESC,
+					"checkedAt" DESC
+			) ph
+			ON
+				ph."postId" = p."id"
+		WHERE
+			${where.join(" AND\n")}
+		GROUP BY
+			p."id",
+			ph."views",
+			ph."kudos",
+			ph."comments"
+		ORDER BY
+			${order}
+		LIMIT 30
+		OFFSET 0
+	`, {
+		type: sequelize.QueryTypes.SELECT,
+		replacements: {
+			query: options.query,
+			before: options.before && options.before.format("YYYY-MM-DD"),
+			after: options.after && options.after.format("YYYY-MM-DD")
+		}
 	});
 
 	ctx.status = 200;
 	ctx.body = {
 		success: true,
-		data: posts
+		options,
+		data: results.map(row => ({
+			...row,
+			tags: row.tags.split(",")
+		}))
 	};
 });
 
