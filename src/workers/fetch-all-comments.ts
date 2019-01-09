35c71f783810c36b62020fb05e5a2f2a02afb965
Ruben Rutten <ruben@rubenrutten.nl>
2019-01-09 16:26:40 +0100

    Added worker to fetch all comments of posts

---
+++ src/workers/fetch-all-comments.ts
@@ -0,0 +1,136 @@
+import "reflect-metadata";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// import all the required modules
+import * as debug from "debug";
+import * as request from "es6-request";
+import { Op } from "sequelize";
+
+// initialize the database
+import "../core/sequelize";
+import "../core/redis";
+
+import PostHistory from "../models/posts/post-history.model";
+import { sequelize } from "../core/sequelize";
+
+const log = debug("app:workers:fetch-all-comments");
+
+log(`started worker`);
+
+// 400 requests per minute
+const rateLimit = 60000 / 400;
+
+const getPosts = async () => {
+	const results = await sequelize.query(`
+		SELECT
+			p."id",
+			p."dumpertId",
+			ph."checkedAt",
+			ph."comments"
+		FROM
+			"posts" p
+		LEFT JOIN
+			"postHistories" ph
+			ON
+				ph."postId" = p."id"
+		WHERE
+			p."postedAt" < '2019-01-01' AND
+			ph."comments" = 0
+		LIMIT 400
+	`, {
+		type: sequelize.QueryTypes.SELECT
+	});
+
+	return results;
+};
+
+const fetchComments = async (post) => {
+	let start = Date.now();
+
+	log(`checking post ${post.dumpertId}`);
+	let [body, res] = await request.get(`https://comments.dumpert.nl/api/v1.0/articles/${post.dumpertId.replace("_", "/")}/comments/?includeitems=0`).header("cookie", "cpc=10; nsfw=1");
+
+	if (res.statusCode !== 200) {
+		log(`status code !== 200`);
+
+		if (body.includes("Article not found")) {
+			log(`post (no longer) found`);
+			PostHistory.update({
+				comments: -1
+			}, {
+				where: {
+					postId: {
+						[Op.eq]: post.id
+					},
+					checkedAt: {
+						[Op.eq]: post.checkedAt
+					}
+				}
+			}).catch((err) => {
+				throw err;
+			});
+
+			return;
+		}
+
+		throw new Error("API returned error");
+	}
+
+	let parsed;
+	try {
+		parsed = JSON.parse(body as string);
+
+		if (parsed.status !== "success") {
+			log(`status !== "success"`);
+			throw new Error(`API returned insuccessful ${JSON.stringify(parsed, null, 2)}`);
+		}
+	} catch (err) {
+		log(`api response not valid json`);
+
+		throw new Error("Response not valid JSON");
+	}
+
+	PostHistory.update({
+		comments: parsed.summary.comment_count
+	}, {
+		where: {
+			postId: {
+				[Op.eq]: post.id
+			},
+			checkedAt: {
+				[Op.eq]: post.checkedAt
+			}
+		}
+	}).catch((err) => {
+		throw err;
+	});
+
+	await new Promise((resolve, reject) => {
+		let duration = Math.max(rateLimit - (Date.now() - start), 0);
+		setTimeout(resolve, duration);
+	});
+};
+
+(async () => {
+	while (true) {
+		try {
+			let posts = await getPosts();
+
+			if (posts.length === 0) {
+				break;
+			}
+
+			for (const post of posts) {
+				console.log(post);
+				await fetchComments(post);
+			}
+		} catch (err) {
+			console.error(`an error occurred: `, err);
+			process.exit(0);
+			break;
+		}
+	}
+
+	log(`done processing`);
+})();
